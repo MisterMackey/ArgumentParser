5d3ab6f6f4379146112f1a7fab6c7973ba8a32cb
Jasper <jasperris93@gmail.com>
2025-06-01 11:05:21 +0200

    testing work, bugs found maybe

--- ArgumentParser.Tests/HelpTextTests.cs
+++ ArgumentParser.Tests/HelpTextTests.cs
@@ -17,7 +17,8 @@ namespace ArgumentParser.Tests
 			// Arrange
 			var builder = new SourceCodeBuilder();
 			builder.AddImports(null)
-				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateAll)]")
+				// we set thrownever to ensure we only test the help text generation, the default err behaviour is to display helptext
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateAll, behaviourOnError: BehaviourOnError.ThrowNever)]")
 				.AddPositionalParameter(new PropertyAndAttributeInfo
 				{
 					PropertyName = "RequiredPositional",
@@ -44,6 +45,137 @@ namespace ArgumentParser.Tests
 			// property for DisplayHelp is generated
 			Assert.Contains("public bool DisplayHelp { get; set; }", output);
 			// helptext is written at some point
+			Assert.Contains("if (instance.DisplayHelp)", output);
+			Assert.Contains("System.Console.WriteLine(TestClass.HelpText);", output);
+		}
+
+		[Fact]
+		public void HelpText_GenerateNone_DoesNotGenerateAnything()
+		{
+			// Arrange
+			var builder = new SourceCodeBuilder();
+			builder.AddImports(null)
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.None, behaviourOnError: BehaviourOnError.ThrowNever)]")
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "RequiredPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", true, 0)
+				}, null)
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "OptionalPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", false, 1)
+				}, null);
+			var source = builder.ToString();
+
+			// Act
+			var (diagnostics, output) = TestHelper.GetGeneratedOutput(source);
+
+			// Assert
+			Assert.Empty(diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+			Assert.DoesNotContain("public const string HelpText =", output);
+			Assert.DoesNotContain("public bool DisplayHelp { get; set; }", output);
+			Assert.DoesNotContain("if (instance.DisplayHelp)", output);
+			Assert.DoesNotContain("System.Console.WriteLine(TestClass.HelpText);", output);
+		}
+
+		[Fact]
+		public void HelpText_GenerateTextOnly_OnlyGeneratesText()
+		{
+			// Arrange
+			var builder = new SourceCodeBuilder();
+			builder.AddImports(null)
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateTextOnly, behaviourOnError: BehaviourOnError.ThrowNever)]")
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "RequiredPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", true, 0)
+				}, null)
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "OptionalPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", false, 1)
+				}, null);
+			var source = builder.ToString();
+
+			// Act
+			var (diagnostics, output) = TestHelper.GetGeneratedOutput(source);
+
+			// Assert
+			Assert.Empty(diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+			Assert.Contains("public const string HelpText =", output);
+			Assert.DoesNotContain("public bool DisplayHelp { get; set; }", output);
+			Assert.DoesNotContain("if (instance.DisplayHelp)", output);
+			Assert.DoesNotContain("System.Console.WriteLine(TestClass.HelpText);", output);
+		}
+
+		[Fact]
+		public void HelpText_GenerateArgumentOnly_OnlyGeneratesArgument()
+		{
+			// Arrange
+			var builder = new SourceCodeBuilder();
+			builder.AddImports(null)
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateArgumentOnly, behaviourOnError: BehaviourOnError.ThrowNever)]")
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "RequiredPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", true, 0)
+				}, null)
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "OptionalPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", false, 1)
+				}, null);
+			var source = builder.ToString();
+
+			// Act
+			var (diagnostics, output) = TestHelper.GetGeneratedOutput(source);
+
+			// Assert
+			Assert.Empty(diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+			Assert.DoesNotContain("public const string HelpText =", output);
+			Assert.Contains("public bool DisplayHelp { get; set; }", output);
+			Assert.DoesNotContain("if (instance.DisplayHelp)", output);
+			Assert.DoesNotContain("System.Console.WriteLine(TestClass.HelpText);", output);
+		}
+
+		[Fact]
+		public void HelpText_GenerateArgumentAndHandler_GeneratesArgumentAndHandlerNoText()
+		{
+			// Arrange
+			var builder = new SourceCodeBuilder();
+			builder.AddImports(null)
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateArgumentAndHandler, behaviourOnError: BehaviourOnError.ThrowNever)]")
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "RequiredPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", true, 0)
+				}, null)
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "OptionalPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", false, 1)
+				}, null)
+				// we need to add the const string here otherwise we error out
+				.AddClassMember("public const string HelpText = \"\";");
+			var source = builder.ToString();
+
+			// Act
+			var (diagnostics, output) = TestHelper.GetGeneratedOutput(source);
+
+			// Assert
+			Assert.Empty(diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+			// this property is there in the source text, but not in the generated output
+			Assert.DoesNotContain("public const string HelpText =", output);
+			Assert.Contains("public bool DisplayHelp { get; set; }", output);
 			Assert.Contains("if (instance.DisplayHelp)", output);
 			Assert.Contains("System.Console.WriteLine(TestClass.HelpText);", output);
 		}
