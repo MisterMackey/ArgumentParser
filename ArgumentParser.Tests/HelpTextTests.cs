f4b185159ae2aa20410d401a6e79464a02c68c03
Jasper <jasperris93@gmail.com>
2025-06-01 10:44:45 +0200

    first test for helptext

--- ArgumentParser.Tests/HelpTextTests.cs
+++ ArgumentParser.Tests/HelpTextTests.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Microsoft.CodeAnalysis;
+using ArgumentParser.Internal;
 
 namespace ArgumentParser.Tests
 {
@@ -14,27 +15,37 @@ namespace ArgumentParser.Tests
 		public void HelpText_GenerateAll_GeneratesHelpTextAndArgument()
 		{
 			// Arrange
-			var source = @"
-using ArgumentParser;
-
-namespace TestNamespace
-{
-    [ParameterCollection(HelpTextGeneration.GenerateAll)]
-    public partial class HelpTextArgs
-    {
-        [Option(""o"", ""Output"", ""Output file path"")]
-        public string OutputPath { get; set; }
-    }
-}";
+			var builder = new SourceCodeBuilder();
+			builder.AddImports(null)
+				.AddClassAttribute("[ParameterCollection(helpTextGeneration: HelpTextGeneration.GenerateAll)]")
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "RequiredPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", true, 0)
+				}, null)
+				.AddPositionalParameter(new PropertyAndAttributeInfo
+				{
+					PropertyName = "OptionalPositional",
+					PropertyType = "string",
+					Attribute = new AttributeInfo("", "", "", false, 1)
+				}, null);
+			var source = builder.ToString();
 
 			// Act
 			var (diagnostics, output) = TestHelper.GetGeneratedOutput(source);
 
 			// Assert
 			Assert.Empty(diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
+			// const string is generated and added
 			Assert.Contains("public const string HelpText =", output);
+			Assert.Contains("Required arguments", output);
+			Assert.Contains("Optional arguments", output);
+			// property for DisplayHelp is generated
 			Assert.Contains("public bool DisplayHelp { get; set; }", output);
-			Assert.Contains("System.Console.WriteLine(HelpTextArgs.HelpText);", output);
+			// helptext is written at some point
+			Assert.Contains("if (instance.DisplayHelp)", output);
+			Assert.Contains("System.Console.WriteLine(TestClass.HelpText);", output);
 		}
 	}
 }
