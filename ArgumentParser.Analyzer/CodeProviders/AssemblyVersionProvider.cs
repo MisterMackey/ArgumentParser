94c97599738fec8e85acce2bf0cd936e583ec07d
Jasper <jasperris93@gmail.com>
2025-06-09 11:12:25 +0200

    adding version provider

---
+++ ArgumentParser.Analyzer/CodeProviders/AssemblyVersionProvider.cs
@@ -0,0 +1,14 @@
+namespace ArgumentParser.Internal;
+
+/// <summary>
+/// This static class provides a SemVer version property to be used by the code generator.
+/// The value will be updated by the build pipeline (in the make release target).
+/// </summary>
+public static class AssemblyVersionProvider
+{
+	/// <summary>
+	/// The fullsemver version of the assembly.
+	/// Updated automatically by the build pipeline.
+	/// </summary>
+	public const string FullSemVer = "1.2.0";
+}
